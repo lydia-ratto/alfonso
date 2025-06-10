ae5cf8cba92c6795976363e038bc08453f4902f8
Lydia Ratto <lydiaratto@gmail.com>
2025-06-10 20:52:54 +0100

    Updated buttongroup to pass direction instead of classname

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         </section>
         <section>
           <p>While gelato simply means ice cream in Italian, we know it’s so much more than that. We use artisan methods to create quality Ice cream that will transport you to Italy. Starting in 2018 from an Ape Piaggio truck in Oxford, we’ve since opened shops around Oxfordshire and neighbouring counties. We hope that our flavours and products can bring comfort, surprise and memorable experiences.</p>
-          <ButtonGroup className="row">
+          <ButtonGroup direction="row">
             <Button variant='primary'>Shops</Button>
             <Button variant='secondary'>Wholesale</Button>
           </ButtonGroup>
